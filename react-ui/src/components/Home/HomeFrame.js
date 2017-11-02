44accf7b1e21b3e4645efb91615dd91fa5bd74ed
cpak11 <c.pak81@gmail.com>
2017-11-02 17:34:40 -0400

    panda stuff

--- react-ui/src/components/Home/HomeFrame.js
+++ react-ui/src/components/Home/HomeFrame.js
@@ -1,45 +1,21 @@
 import React, { Component } from 'react'
+import { HashRouter as Router, Route, Switch, Link } from "react-router-dom"
+
+import GamingNews from './GamingNews/GamingNews'
+import EsportEvents from './EsportEvents/EsportEvents'
 
 import '../../styles/Home/HomeFrame.css'
 
-const gamingUrl = 'https://newsapi.org/v1/articles?source=polygon&sortBy=top&apiKey=2107a41c7d9549d497b83f34f6f8aa6b';
 
 class HomeFrame extends Component{
-
-	constructor(props){
-		super(props)
-		this.state = {
-			news: {articles:[]}
-		}
-	}
-
-	componentDidMount(){
-		return fetch(gamingUrl)
-		.then( resp => resp.json() )
-		.then( re => {
-			this.setState({
-				news:re
-			})
-		})	
-	}
 
 	render(){
 		return(
 			<div id="HomeFrame">
 
-				<h1>Home Frame</h1>
+				<EsportEvents/>				
 
-				<div id="gamingNews">
-					<h2>Today In Gaming:</h2>
-					{
-						this.state.news.articles.map( (article)=>{
-							return <div>
-								<h4>{article.title} - {article.author}</h4>
-								<img src={article.urlToImage} alt={'img'}/>
-							</div>
-						})
-					}
-				</div>
+				<GamingNews />
 
 				<div id="socialMediaSection">
 					<div id="tweets">
