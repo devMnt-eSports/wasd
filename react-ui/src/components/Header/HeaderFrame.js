864311544a5e84be019acea95f0a2c15ef433926
Stewart Atwood <stewartatwood@Stewarts-MacBook-Pro.local>
2017-11-02 09:35:44 -0500

    fixed broken apostrophe in header

--- react-ui/src/components/Header/HeaderFrame.js
+++ react-ui/src/components/Header/HeaderFrame.js
@@ -1,46 +1,56 @@
-import React, { Component } from 'react'
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import NavBarFrame from '../../components/Header/NavBar/NavBarFrame';
+import NavBarFrame from "../../components/Header/NavBar/NavBarFrame";
 
-import '../../styles/Header/HeaderFrame.css';
+import "../../styles/Header/HeaderFrame.css";
 
+import wasdlogo from "../../assets/wasd.png";
 
-import wasdlogo from '../../assets/wasd.png'
+class HeaderFrame extends Component {
+  render() {
+    return (
+      <div id="HeaderFrame">
+        <div className="wasd_div">
+          <Router>
+            <Link to={"/"}>
+              <img src={wasdlogo} alt={"wasd logo"} />
+            </Link>
+          </Router>
+        </div>
 
-class HeaderFrame extends Component{
-	render(){
-		return(
-			<div id="HeaderFrame">
-				<div className="wasd_div">
-					<Router><Link to={'/'}><img src={wasdlogo} alt={'wasd logo'} /></Link></Router>
-				</div>
+        <div className="navGamesDiv">
+          <div className="navGame">
+            <h3>PLAYERUNKNOWN{`'`}S BATTLEGROUNDS</h3>
+          </div>
+          <div className="navGame">
+            <h3>LEAGUE OF LEGENDS</h3>
+          </div>
+          <div className="navGame">
+            <h3>OVERWATCH</h3>
+          </div>
+        </div>
 
-				<div className="navGamesDiv">
-					<div className="navGame">
-						<h3>PLAYERUNKNOWN'S BATTLEGROUNDS</h3>
-					</div>
-					<div className="navGame">
-						<h3>LEAGUE OF LEGENDS</h3>
-					</div>
-					<div className="navGame">
-						<h3>OVERWATCH</h3>
-					</div>
-				</div>
-
-				{/*
+        {/*
 				<NavBarFrame />
 			*/}
 
-				<ul>
-					<li><Router><Link to={'/profile'}>My Profile</Link></Router></li>
-					<li><Router><Link to={'/forums'}>Forums</Link></Router></li>
-				</ul>
-
-			</div>
-		)
-	}
+        <ul>
+          <li>
+            <Router>
+              <Link to={"/profile"}>My Profile</Link>
+            </Router>
+          </li>
+          <li>
+            <Router>
+              <Link to={"/forums"}>Forums</Link>
+            </Router>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default HeaderFrame
+export default HeaderFrame;
