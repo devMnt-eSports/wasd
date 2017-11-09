139aec11b586f81c9304f68108036ab8c228d1bd
Stewart Atwood <stewartatwood@Stewarts-MacBook-Pro.local>
2017-11-09 11:00:50 -0600

    starting work on profile css

--- react-ui/src/components/Profile/ProfileFrame.js
+++ react-ui/src/components/Profile/ProfileFrame.js
@@ -77,34 +77,37 @@ class ProfileFrame extends Component {
 
       myPosts = postsArr.reverse().map((e, i) => {
         return (
-          <div className="example-post" key={i}>
-            <div className="flex-post">
-              <h1> {e.title} </h1>
-              <img
-                id="profile-resizer"
-                src={
-                  e.user_profile_pic ||
-                  "https://vignette.wikia.nocookie.net/jamesbond/images/6/61/Generic_Placeholder_-_Profile.jpg/revision/latest?cb=20121227201208"
-                }
-              />
-            </div>
-            <div>
-              <p>
-                posted by <b>{e.user_name}</b>
-              </p>
-              <p>{e.content}</p>
-              {/* <div>{commentSection}</div> */}
-              <input
-                id="comment-input"
-                type="text"
-                placeholder="Leave comment..."
-                onChange={e => this.handleCommentChange(e)}
-              />
-              <button onClick={event => this.postComment(event)}>
-                Comment
-              </button>
-              <button>Upvote</button>
-              <button>Report</button>
+          <div className="flex-personal" key={i}>
+            <div className="center-personal">
+              <div className="flex-my-posts">
+                <h1> {e.title} </h1>
+                <div id="personal-resizer">
+                  <img
+                    src={
+                      e.user_profile_pic ||
+                      "https://vignette.wikia.nocookie.net/jamesbond/images/6/61/Generic_Placeholder_-_Profile.jpg/revision/latest?cb=20121227201208"
+                    }
+                  />
+                </div>
+              </div>
+              <div>
+                <p>
+                  posted by <b>{e.user_name}</b>
+                </p>
+                <p>{e.content}</p>
+                {/* <div>{commentSection}</div> */}
+                <input
+                  id="comment-input"
+                  type="text"
+                  placeholder="Leave comment..."
+                  onChange={e => this.handleCommentChange(e)}
+                />
+                <button onClick={event => this.postComment(event)}>
+                  Comment
+                </button>
+                <button>Upvote</button>
+                <button>Report</button>
+              </div>
             </div>
           </div>
         );
@@ -118,15 +121,16 @@ class ProfileFrame extends Component {
             <h3>
               Welcome, <b>{this.state.user.name || "current user"}</b>!
             </h3>
-            <img
-              id="profile-resizer"
-              src={
-                this.state.user.user_profile_pic ||
-                this.state.imagePreviewUrl ||
-                "https://vignette.wikia.nocookie.net/jamesbond/images/6/61/Generic_Placeholder_-_Profile.jpg/revision/latest?cb=20121227201208"
-              }
-              alt={this.state.user.name}
-            />
+            <div id="personal-resizer">
+              <img
+                src={
+                  this.state.user.user_profile_pic ||
+                  this.state.imagePreviewUrl ||
+                  "https://vignette.wikia.nocookie.net/jamesbond/images/6/61/Generic_Placeholder_-_Profile.jpg/revision/latest?cb=20121227201208"
+                }
+                alt={this.state.user.name}
+              />
+            </div>
             <form onSubmit={event => this.uploadImage(event)}>
               <input
                 type="file"
